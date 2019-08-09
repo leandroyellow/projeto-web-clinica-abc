78df4dbfce17d13528560649d234b315f69dd27b
leandro.qtrepador <leandro.biologo@yahoo.com.br>
2019-08-09 09:00:31 -0300

    header

--- index.php
+++ index.php
@@ -12,17 +12,19 @@
 </head>
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light container">
-            <a class="navbar-brand" href="#"><img src="imagens/logopq2.png" alt=""> <span class="ml-2">Clinica ABC</span></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav ml-auto">
-                    <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                    <a class="nav-item nav-link" href="#">Planos</a>
-                    <a class="nav-item nav-link" href="#">Contato</a>
-                    <a class="nav-item nav-link" href="#">Entrar</a>
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="#"><img src="imagens/logopq2.png" alt=""> <span class="ml-2">Clinica ABC</span></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav ml-auto">
+                        <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-item nav-link" href="#">Planos</a>
+                        <a class="nav-item nav-link" href="#">Contato</a>
+                        <a class="nav-item nav-link" href="#">Entrar</a>
+                    </div>
                 </div>
             </div>
         </nav>
