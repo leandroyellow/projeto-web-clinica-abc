6d049edf59820076a72ef08c290ff0d62bb87077
leandroyellow <leandro.biologo@yahoo.com.br>
2019-08-08 23:47:30 -0300

    especialidades agenda

--- index.php
+++ index.php
@@ -33,7 +33,16 @@
         <div class="center">
             <form>
                 <h2>Agende sua consulta online</h2>
-                <input type="email" name="email" require>
+                <div class="form-group">
+                    <label for="exampleFormControlSelect1">Especialidades</label>
+                    <select class="form-control" id="exampleFormControlSelect1">
+                        <option>Clínico Geral</option>
+                        <option>Giicologista</option>
+                        <option>Pediatra</option>
+                        <option>Nutricionista</option>
+                        <option>Dentista</option>
+                    </select>
+                </div>
                 <input type="submit" name="acao" value="Agendar">
             </form>
         </div><!--center-->
@@ -124,5 +133,9 @@
         
         
     </footer>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
