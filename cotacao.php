915d091f2eb621ebf7330126314bbe384265c155
leandro.qtrepador <leandro.biologo@yahoo.com.br>
2019-09-10 11:49:13 -0300

    phpmailer

--- cotacao.php
+++ cotacao.php
@@ -3,14 +3,14 @@
 <div class="cor">
     <div class="container">
         <h2 class="text-center sucesso">Faça sua Cotação</h2>
-        <form action="">
+        <form action="email.php" method="post">
             <div class="form-group col-md-12">
                 <label for="campoEmail">Email:</label>
                 <input type="email" class="form-control" name="email" id="campoEmail" placeholder="Digite seu email" autocomplete="off" required>
             </div>
             <div class="form-group col-md-12">
                 <label for="campoDescricao">Descrição:</label>
-                <textarea class="form-control" id="campoDescricao" rows="5"></textarea><br>
+                <textarea class="form-control" id="campoDescricao" rows="5" name="descricao" required></textarea>
             </div>
             <div class="text-right form-group col-md-12">
                 <a href="index.php" id="botao" class="btn" >Voltar</a>
