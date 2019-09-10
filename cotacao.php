c6495fe91930930e504f827a2159000cab992310
leandroyellow <leandro.biologo@yahoo.com.br>
2019-09-10 19:48:08 -0300

    phpmailer

--- cotacao.php
+++ cotacao.php
@@ -1,9 +1,17 @@
-<?php include("header_paciente.php"); ?>
+<?php include("header_paciente.php"); 
+//BUSCANDO A CLASSE
+require_once 'PHPMailer/Funcoes.class.php';
+//ESTANCIANDO A CLASSE
+$objFc = new Funcoes();
+if(isset($_POST['btEnviar'])){
+	$objFc->enviarEmail($_POST);
+}
+?>
 
 <div class="cor">
     <div class="container">
         <h2 class="text-center sucesso">Faça sua Cotação</h2>
-        <form action="email.php" method="post">
+        <form action="" method="post">
             <div class="form-group col-md-12">
                 <label for="campoEmail">Email:</label>
                 <input type="email" class="form-control" name="email" id="campoEmail" placeholder="Digite seu email" autocomplete="off" required>
@@ -14,7 +22,7 @@
             </div>
             <div class="text-right form-group col-md-12">
                 <a href="index.php" id="botao" class="btn" >Voltar</a>
-                <button type="submit" class="btn" id="botao">Enviar</button>
+                <button type="submit" class="btn" id="botao" name="btEnviar">Enviar</button>
             </div>
         </form>
     </div>
