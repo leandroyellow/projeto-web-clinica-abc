608f31e93d85556a023853b624f840ff03da9cc1
leandroyellow <leandro.biologo@yahoo.com.br>
2019-09-18 14:05:41 -0300

    s

--- profissional_editar.php
+++ profissional_editar.php
@@ -11,17 +11,28 @@ $celular = $_POST['celular'];
 $especialidade = $_POST['especialidade'];
 //$arquivo = $_POST['arquivo'];
 
-$extensao = strtolower(substr($_FILES['arquivo']['name'], -4)); //pega a extensao do arquivo
-$novo_nome = md5(time()) . $extensao; //define o nome do arquivo
-$diretorio = "upload/"; //define o diretorio para onde enviaremos o arquivo
-move_uploaded_file($_FILES['arquivo']['tmp_name'], $diretorio.$novo_nome); //efetua o upload
+if(isset($_FILES['arquivo'])) {
+    $extensao = strtolower(substr($_FILES['arquivo']['name'], -4)); //pega a extensao do arquivo
+    $novo_nome = md5(time()) . $extensao; //define o nome do arquivo
+    $diretorio = "upload/"; //define o diretorio para onde enviaremos o arquivo
+    move_uploaded_file($_FILES['arquivo']['tmp_name'], $diretorio.$novo_nome); //efetua o upload
 
-$sql = "UPDATE usuario SET usuario.email='$email'   WHERE usuario.id = '$id'";
-$resultado = $conexao->query($sql);
+    $sql = "UPDATE usuario SET usuario.email='$email'   WHERE usuario.id = '$id'";
+    $resultado = $conexao->query($sql);
 
-$sql2 = "UPDATE profissional SET profissional.nome='$nome', profissional.especialidade='$especialidade', profissional.celular='$celular', profissional.registro='$registro', profissional.arquivo='$novo_nome' WHERE profissional.usuario_id = $id";
-$resultado2 = $conexao->query($sql2);
+    $sql2 = "UPDATE profissional SET profissional.nome='$nome', profissional.especialidade='$especialidade', profissional.celular='$celular', profissional.registro='$registro', profissional.arquivo='$novo_nome' WHERE profissional.usuario_id = $id";
+    $resultado2 = $conexao->query($sql2);
 
-header('Location: listar_cadastro.php');
+    header('Location: listar_cadastro.php');
+}
+else{
+    $sql = "UPDATE usuario SET usuario.email='$email'   WHERE usuario.id = '$id'";
+    $resultado = $conexao->query($sql);
+
+    $sql2 = "UPDATE profissional SET profissional.nome='$nome', profissional.especialidade='$especialidade', profissional.celular='$celular', profissional.registro='$registro' WHERE profissional.usuario_id = $id";
+    $resultado2 = $conexao->query($sql2);
+
+    //header('Location: listar_cadastro.php');
+}
  
 ?>
